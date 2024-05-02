32bf0f807b881cab04a24487f7866f178c4d319e
Marketing 2 <corporativo.hm.next2@gmail.com>
2024-05-02 15:14:06 -0600

    Se agrega responsive e icono.png

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
 import Formulario from "./components/Fomulario";
 const App = () => {
   return (
-    <div className=" bg-slate-50 container border-2 h-screen border-green-600 flex flex-col justify-center items-center px-10">
+    <div className="bg-amber-100 w-full h-screen flex flex-col justify-center items-center px-10">
       <Formulario />
     </div>
   );
--- src/components/Fomulario.jsx
+++ src/components/Fomulario.jsx
@@ -13,6 +13,7 @@ const Formulario = () => {
         }
         else {
             alert('Subiendo Archivo');
+            setImage(null)
         }
     }
 
@@ -31,8 +32,8 @@ const Formulario = () => {
     }
 
     return (
-        <form action="" onSubmit={handleSubmit} className="container flex justify-center items-center gap-7 flex-col">
-            <div className="border border-dashed border-blue-400 h-56 w-3/4 flex justify-center items-center ">
+        <form action="" onSubmit={handleSubmit} className="container flex justify-center items-center gap-7 flex-col md:w-1/3 bg-white p-6 shadow-md rounded-md">
+            <div className="h-56 w-3/4 flex justify-center items-center md:border md:border-blue-700 md:border-dashed md:w-full">
                 {
                     image
                         ?
@@ -44,10 +45,10 @@ const Formulario = () => {
                 }
             </div>
 
-            <div className="w-3/4 flex flex-col gap-5">
-                <label htmlFor="file" className="hover:bg-sky-800 font-medium p-2 text-center bg-sky-500 text-white rounded-md">Seleccione un Archivo</label>
+            <div className="w-3/4 flex flex-col gap-5 md:w-full">
+                <label htmlFor="file" className="hover:bg-sky-600 transition-all duration-150 hover:shadow-md  font-medium p-2 text-center bg-sky-500 text-white rounded-md cursor-pointer">Seleccione un Archivo</label>
                 <input onChange={handleChange} type="file" name="file" id="file" className="hidden" />
-                <button type="submit" className="hover:bg-gray-700 font-medium p-2 text-center bg-gray-500 text-white rounded-md">Enviar</button>
+                <button type="submit" className="hover:bg-gray-600  transition-all duration-150 hover:shadow-md font-medium p-2 text-center bg-gray-500 text-white rounded-md  cursor-pointer">Enviar</button>
             </div>
         </form>
     );
